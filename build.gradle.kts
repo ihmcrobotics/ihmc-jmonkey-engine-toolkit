93e29a6c797844449bc157f6af9ef7015d5ad703
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-02 11:19:12 -0600

    :arrow_up: ihmc-graphics-description 0.19.2

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,7 +43,7 @@ mainDependencies {
    api("us.ihmc:euclid:0.15.2")
    api("us.ihmc:euclid-shape:0.15.2")
    api("us.ihmc:ihmc-commons:0.30.4")
-   api("us.ihmc:ihmc-graphics-description:0.19.1")
+   api("us.ihmc:ihmc-graphics-description:0.19.2")
 }
 
 testDependencies {
