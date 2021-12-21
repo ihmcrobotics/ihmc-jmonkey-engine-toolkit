ac362aff65511bd0a61af158c76251ec3063c1ca
Duncan Calvert <dcalvert@ihmc.us>
2021-12-20 20:31:00 -0600

    :arrow_up: ihmc-commons 0.30.5 upgraded dependencies including log4j fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -42,10 +42,10 @@ mainDependencies {
 
    api("us.ihmc:euclid:0.17.0")
    api("us.ihmc:euclid-shape:0.17.0")
-   api("us.ihmc:ihmc-commons:0.30.4")
+   api("us.ihmc:ihmc-commons:0.30.5")
    api("us.ihmc:ihmc-graphics-description:0.19.4")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.30.4")
+   api("us.ihmc:ihmc-commons-testing:0.30.5")
 }
