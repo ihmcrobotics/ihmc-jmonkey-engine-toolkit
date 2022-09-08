f1e787ad4611a6ad7360d8fbbf95f6c42e2508af
Sylvain Bertrand <sbertrand@ihmc.org>
2022-09-08 16:07:59 -0500

    :arrow_up: euclid 0.19.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,8 +40,8 @@ mainDependencies {
    api("com.vividsolutions:jts:1.13")
    api("com.google.guava:guava:18.0")
 
-   api("us.ihmc:euclid:0.18.1")
-   api("us.ihmc:euclid-shape:0.18.1")
+   api("us.ihmc:euclid:0.19.0")
+   api("us.ihmc:euclid-shape:0.19.0")
    api("us.ihmc:ihmc-commons:0.31.0")
    api("us.ihmc:ihmc-graphics-description:0.19.7")
 }
